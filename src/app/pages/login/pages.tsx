850903255d38576b0b3b06548cd0bad34775b6ea
Odouglas2003 <octaviodouglas200@gmail.com>
2025-01-17 15:29:25 -0300

    login echo

---
+++ src/app/pages/login/pages.tsx
@@ -0,0 +1,10 @@
+"use client"
+import Lo from "../../../../components/login/logins"
+export default function login(){
+    return(<>
+    <section style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100%"  }}>
+    <Lo /> 
+    </section>
+    </>
+    )
+}
